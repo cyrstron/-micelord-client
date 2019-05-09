634366100a2cc69403290e7c1d01b0cf4f7e7b7e
Kyryl Stronko <Kyryl.Stronko@logianalytics.com>
2019-05-09 22:54:24 +0300

    distrib points

--- src/components/maps/components/grid-overlay/grid-overlay.tsx
+++ src/components/maps/components/grid-overlay/grid-overlay.tsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {SmartTilesOverlay} from '@maps/tiles-overlay';
 import {StaticGrider, createBorderRenderer, BorderRenderer} from '@micelord/grider';
 import {GridTile} from '../grid-tile/grid-tile';
-import {SmartMarker} from '@maps/feature';
+import {SmartMarker, SmartPolyline} from '@maps/feature';
 
 interface Props {
   grider: StaticGrider;
@@ -12,7 +12,7 @@ interface Props {
 
 export class GridOverlay extends Component<Props> {
   borderRenderer: BorderRenderer;
-  // distribution: {[key: string]: google.maps.LatLngLiteral[]};
+  distribution: {point: google.maps.LatLngLiteral, index: number}[][];
 
   constructor(props: Props) {
     super(props);
@@ -24,7 +24,7 @@ export class GridOverlay extends Component<Props> {
     } = props;
 
     this.borderRenderer = createBorderRenderer(grider.params, borderline, border);
-    // this.distribution = this.borderRenderer.distributePoints();
+    this.distribution = this.borderRenderer.distributePoints();
   }
 
   shouldComponentUpdate(nextProps: Props) {
@@ -43,12 +43,40 @@ export class GridOverlay extends Component<Props> {
     } = this.props;
     return (
       <>
-      {/* {this.distribution[0].map((point, index) => (
+      {this.distribution[0].map(({point, index}) => (
         <SmartMarker 
           position={point}
           title={`${index}`}
         />
-      ))} */}
+      ))}
+      {/* <SmartMarker 
+        position={{lat: 54.2285004, lng: 24.7708037}}
+        title={'lal'}
+      /> */}
+      <SmartPolyline 
+        path={this.distribution[0].map(({point}) => point)}
+        strokeColor='#ff0000'
+      />
+      <SmartPolyline 
+        path={this.distribution[1].map(({point}) => point)}
+        strokeColor='#00ff00'
+      />
+      <SmartPolyline 
+        path={this.distribution[2].map(({point}) => point)}
+        strokeColor='#0000ff'
+      />
+      <SmartPolyline 
+        path={this.distribution[3].map(({point}) => point)}
+        strokeColor='#ff00ff'
+      />
+      <SmartPolyline 
+        path={this.distribution[4].map(({point}) => point)}
+        strokeColor='#ffff00'
+      />
+      <SmartPolyline 
+        path={this.distribution[5].map(({point}) => point)}
+        strokeColor='#ffffff'
+      />
       <SmartTilesOverlay width={512}>
         {(tileConfig) => (
           <GridTile 
